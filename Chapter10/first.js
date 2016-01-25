a9fbbc6a12b27190735daf47e9ac9f42d213d92a
upuldi <upuldi@gmail.com>
2016-01-25 23:35:49 +1100

    add

---
+++ Chapter10/first.js
@@ -0,0 +1,36 @@
+var a = 10,b='2';
+console.log(a+b);
+
+console.log('Second Example');
+
+var a = 20; b = '30' , c = 3;
+console.log(a*c*b); // Here b is auto converted into a number by the JS engine.
+
+console.log('Operator example');
+var value = 1<2<3;
+console.log('1<2<3 result is : ' + value);
+value = 3<2<1;
+console.log('3<2<1 result is : ' + value);
+console.log('True number value : ' + Number(true));
+console.log('False number value : ' + Number(false));
+
+console.log('Number Coercion');
+console.log('Number(undefined) : ' + Number(undefined));
+console.log('Number(null) : ' + Number(null));
+console.log('Number(string) : ' + Number('String'));
+
+console.log('Issues with equal == operator strage beaviour due to coercion ');
+value = '1' == 0;
+console.log('1 == \'0\' : ' + value);
+value = '1' == 1;
+console.log('1 == \'1\' : ' + value);
+value = '1' == true;
+console.log('1 == true : ' + value);
+value = '1' == false;
+console.log('1 == false : ' + value);
+value = '0' == null;
+console.log('1 == null : ' + value);
+value = '0' == '';
+console.log(' \'0\' == \'\'  : ' + value);
+value = 0 == '';
+console.log('0 == \'\'  : ' + value);
---
+++ Chapter9/first.js
@@ -0,0 +1,20 @@
+// js is diyanamically typed
+
+console.log('Diyanamically typed nature of java scripts');
+
+var x = 1;
+console.log(x);
+x = 'String Val';
+console.log(x);
+x = true;
+console.log(x);
+
+
+//JS Prmitive types
+
+var a = undefined;
+var b = null; 
+var c = true ; //boolean type
+var d = 100; // number - only one numaric type. this will always be a floating point number with decimals attached.
+var e = 'String Value' + "String again";
+var symbol; // This primitive type will be used in ES6
